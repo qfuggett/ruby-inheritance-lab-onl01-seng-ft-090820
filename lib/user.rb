83e5012517a82e04eac20f319574f22ee312f378
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-26 17:07:36 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,10 @@
 class User
   attr_accessor :first_name, :last_name
   
-  # def initialize(firstname, lastname)
-  #   @firstname = firstname
-  #   @lastname = lastname
-  # end
+  def initialize(firstname, lastname)
+    @firstname = firstname
+    @lastname = lastname
+  end
   
   
 end
