ced9fbcb94d8dc9da913bab9cb5b5917c3f070db
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-26 17:05:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
 class Student < User
+  attr_accessor :knowledge
+
+  @@knowledge = [ ]
+
+  def initialize(knowledge)
+    @knowledge = knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
