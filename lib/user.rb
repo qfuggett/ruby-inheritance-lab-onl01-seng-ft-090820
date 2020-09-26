0a7ae36a23274c5c82ca5a4f57851cb9c43bad45
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-26 17:12:45 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,6 +2,7 @@ class User
   attr_accessor :first_name, :last_name
   
   def initialize(first_name = nil, last_name = nil)
+    #if arguments are not set to nil default, #teacher will not work
     @first_name = first_name
     @last_name = last_name
   end
